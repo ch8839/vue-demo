eaf7c27633378e297e0f14dbfedf8807a65fd1d8
士心于心 <993634344@qq.com>
2019-03-10 22:02:40 +0800

    sql修改

--- server/sql/list2.sql
+++ server/sql/list2.sql
@@ -1,8 +1,41 @@
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (1, '刚', 19951030, '男');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (2, 'admin', 123456, '男');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (3, 'tourist', 111, '女');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (4, '谢晋飞', 1001, '女');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (5, '吴迪嘉', 1002, '男');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (6, '王妍', 1003, '女');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (7, '刘千红', 1004, '女');
-INSERT INTO `list2` (`id`, `username`, `number`, `sex`) VALUES (8, '陈刚', 1005, '男');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50561
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50561
+File Encoding         : 65001
+
+Date: 2019-03-10 21:54:40
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for list2
+-- ----------------------------
+DROP TABLE IF EXISTS `list2`;
+CREATE TABLE `list2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `number` int(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of list2
+-- ----------------------------
+INSERT INTO `list2` VALUES ('1', '刚', '19951030', '男');
+INSERT INTO `list2` VALUES ('2', 'admin', '123456', '男');
+INSERT INTO `list2` VALUES ('3', 'tourist', '111', '女');
+INSERT INTO `list2` VALUES ('4', '谢晋飞', '1001', '女');
+INSERT INTO `list2` VALUES ('5', '吴迪嘉', '1002', '男');
+INSERT INTO `list2` VALUES ('6', '王妍', '1003', '女');
+INSERT INTO `list2` VALUES ('7', '刘千红', '1004', '女');
+INSERT INTO `list2` VALUES ('8', '陈刚', '1005', '男');
+SET FOREIGN_KEY_CHECKS=1;
--- server/sql/user.sql
+++ server/sql/user.sql
@@ -1,3 +1,35 @@
-INSERT INTO `user` (`id`, `username`, `password`) VALUES (1, '刚', '19951030');
-INSERT INTO `user` (`id`, `username`, `password`) VALUES (2, 'admin', '123456');
-INSERT INTO `user` (`id`, `username`, `password`) VALUES (3, 'tourist', '111');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50561
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50561
+File Encoding         : 65001
+
+Date: 2019-03-10 21:54:53
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '刚', '19951030');
+INSERT INTO `user` VALUES ('2', 'admin', '123456');
+INSERT INTO `user` VALUES ('3', 'tourist', '111');
+SET FOREIGN_KEY_CHECKS=1;
